0f7facd1afdc2ac2e9df21f2a512d76ac4d97a84
Aditya khadka <adityakhadka907@gmail.com>
2021-07-02 12:05:24 +0545

    commit

--- Geet/Project/community/admin.py
+++ Geet/Project/community/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Postmodel
+
 
 # Register your models here.
+
+
+admin.site.register(Postmodel)
--- Geet/Project/community/models.py
+++ Geet/Project/community/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.enums import Choices
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Postmodel(models.Model):
+    STATUS_CHOICES=( ('draft','Draft'),('published','Published'),
+                    ) 
+
+    title=models.CharField(max_length=200)
+    slug=models.SlugField(unique_for_date='publish',null=True,blank=True)
+    author=models.ForeignKey(User,on_delete=CASCADE,related_name='author_posts')
+    body=models.TextField()
+    image=models.ImageField(upload_to='images')
+    publish=models.DateTimeField(default=timezone.now)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    status=models.CharField(max_length=20,choices=STATUS_CHOICES,default="draft")             
+
+    class meta:
+        ordering=('-publish',)  #sort post according to the publish date
+
+
+    def __str__(self):
+        return self.title    
+
+    def save(self , *args, **kwargs):
+        if not self.slug: 
+          self.slug = slugify(self.title)
+        super(Postmodel, self).save(*args, **kwargs)    
